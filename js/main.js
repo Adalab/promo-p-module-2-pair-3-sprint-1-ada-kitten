83936cf0ba80f50dc62516468a794e98a64161e9
LorenaGomezPerez <lorenagomezperez@gmail.com>
2022-03-21 19:50:26 +0100

    add exercise module 2.2 js

--- js/main.js
+++ js/main.js
@@ -57,3 +57,82 @@ contentLi_003=`<li class="card card_003">
 
 content = contentLi_001 + contentLi_002 + contentLi_003;
 catList.innerHTML = content;
+
+
+// exercise 2.2. Trabajar con variables e interpolarlas y Bonus: Convertir cadenas de carácteres.
+
+//kittenone
+
+const kittenImageOne = "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg";
+const kittenNameOne = "anastacio";
+//mayúsculas
+const nameCapitalizedOne = kittenNameOne.charAt(0).toUpperCase() + kittenNameOne.slice(1);
+const kittenDescOne =
+  "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+const kittenRaceOne = "British Shorthair";
+
+//kittentwo
+
+const kittenImageTwo = "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg";
+const kittenNameTwo = "fiona";
+//mayúsculas
+const nameCapitalizedTwo = kittenNameTwo.charAt(0).toUpperCase() + kittenNameTwo.slice(1);
+const kittenDescTwo =
+
+  "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+const kittenRaceTwo = "British Shorthair";
+
+//kittenthree
+
+const kittenImageThree = "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg";
+const kittenNameThree = "cielo";
+//mayúsculas
+const nameCapitalizedThree = kittenNameThree.charAt(0).toUpperCase() + kittenNameThree.slice(1);
+const kittenDescThree =
+  "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+const kittenRaceThree = "British Shorthair";
+
+
+
+const list = document.querySelector (".list");
+
+
+
+list.innerHTML= `<li class="card">
+<article>
+  <img
+    class="card_img"
+    src="${kittenImageOne}"
+    alt="gatito"
+  />
+  <h3 class="card_title">${nameCapitalizedOne}</h3>
+  <h4 class="card_race">${kittenRaceOne}</h4>
+  <p class="card_description">
+    ${kittenDescOne}
+  </p>
+</article>
+</li>
+<li class="card">
+<img
+  class="card_img"
+  src=${kittenImageTwo}
+  alt="gatito"
+/>
+<h3 class="card_title">${nameCapitalizedTwo}</h3>
+<h4 class="card_race">${kittenRaceTwo}</h4>
+<p class="card_description">
+${kittenDescTwo}
+</p>
+</li>
+<li class="card">
+ <img
+class="card_img"
+src=${kittenImageThree}
+alt="gatito"/>
+<h3 class="card_title">${nameCapitalizedThree}</h3>
+<h4 class="card_race">${kittenRaceThree}</h4>
+<p class="card_description">
+${kittenDescThree}
+</p>
+</li>
+`;
